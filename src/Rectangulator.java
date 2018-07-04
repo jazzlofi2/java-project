f5f15f72dfda61afe0f41e879cfad423e5706973
user <user@user.com>
2018-07-04 14:29:57 +0300

    initial java commit

---
+++ src/Rectangulator.java
@@ -0,0 +1,11 @@
+public class Rectengulator {
+
+    public static void main(String[] args) {
+        int length = Integer.parseInt(args[0]);
+        int width = Interger.parseInt(args[1]);
+        Rectangle myRectangle = new Rectangle(length, width)
+        String output = "Rec length: " + myRectangle.length;
+        System.out.println(output);
+    }
+}
+
