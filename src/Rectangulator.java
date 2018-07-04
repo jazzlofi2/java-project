a90bd1dc7bdb6b99c034e2b70c8d91042b7528cd
mrjenkins@jenkins.com <jenkins@jenkins.com>
2018-07-04 16:31:38 +0300

    typo

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -2,7 +2,7 @@ public class Rectengulator {
 
     public static void main(String[] args) {
         int length = Integer.parseInt(args[0]);
-        int width = Interger.parseInt(args[1]);
+        int width = Integer.parseInt(args[1]);
         Rectangle myRectangle = new Rectangle(length, width);
         String output = "Rec length: " + myRectangle.length;
         System.out.println(output);
